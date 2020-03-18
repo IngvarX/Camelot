7e284dde4ece5535baa802e8088700a9d3c19731
ingvar1995 <igorn1995@yandex.ru>
2020-03-19 01:23:24 +0300

    Added operation service tests

---
+++ Camelot.Tests/OperationsServiceTests.cs
@@ -0,0 +1,182 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Camelot.Services.Implementations;
+using Camelot.Services.Interfaces;
+using Camelot.Services.Operations.Interfaces;
+using Camelot.Services.Operations.Settings;
+using Moq;
+using Xunit;
+
+namespace Camelot.Tests
+{
+    public class OperationsServiceTests
+    {
+        private const string FileName = "FileName";
+        private const string SelectedDirectoryName = "SelectedDirectoryName";
+        private const string DirectoryName = "DirectoryName";
+
+        [Fact]
+        public void TestEditSelectedFiles()
+        {
+            var filesSelectionServiceMock = new Mock<IFilesSelectionService>();
+            filesSelectionServiceMock
+                .SetupGet(m => m.SelectedFiles)
+                .Returns(new[] {FileName});
+
+            var operationsFactoryMock = new Mock<IOperationsFactory>();
+            var directoryServiceMock = new Mock<IDirectoryService>();
+            var fileOpeningServiceMock = new Mock<IFileOpeningService>();
+            fileOpeningServiceMock
+                .Setup(m => m.Open(FileName))
+                .Verifiable();
+
+            IOperationsService operationsService = new OperationsService(
+                filesSelectionServiceMock.Object,
+                operationsFactoryMock.Object,
+                directoryServiceMock.Object,
+                fileOpeningServiceMock.Object);
+
+            operationsService.EditSelectedFiles();
+
+            fileOpeningServiceMock.Verify(m => m.Open(FileName), Times.Once());
+        }
+
+        [Fact]
+        public async Task TestFilesRemoving()
+        {
+            var filesSelectionServiceMock = new Mock<IFilesSelectionService>();
+            filesSelectionServiceMock
+                .SetupGet(m => m.SelectedFiles)
+                .Returns(new[] {FileName});
+
+            var operationsFactoryMock = new Mock<IOperationsFactory>();
+            var operationMock = new Mock<IOperation>();
+            operationMock
+                .Setup(m => m.RunAsync(It.IsAny<CancellationToken>()))
+                .Verifiable();
+            operationsFactoryMock
+                .Setup(m => m.CreateDeleteOperation(It.IsAny<IList<UnaryFileOperationSettings>>()))
+                .Callback<IList<UnaryFileOperationSettings>>(l =>
+                {
+                    Assert.Equal(FileName, l.Single().FilePath);
+                })
+                .Returns(operationMock.Object);
+
+            var directoryServiceMock = new Mock<IDirectoryService>();
+            var fileOpeningServiceMock = new Mock<IFileOpeningService>();
+
+            IOperationsService operationsService = new OperationsService(
+                filesSelectionServiceMock.Object,
+                operationsFactoryMock.Object,
+                directoryServiceMock.Object,
+                fileOpeningServiceMock.Object);
+
+            await operationsService.RemoveSelectedFilesAsync();
+
+            operationMock.Verify(m => m.RunAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task TestFilesMove()
+        {
+            var filesSelectionServiceMock = new Mock<IFilesSelectionService>();
+            filesSelectionServiceMock
+                .SetupGet(m => m.SelectedFiles)
+                .Returns(new[] {FileName});
+
+            var operationsFactoryMock = new Mock<IOperationsFactory>();
+            var operationMock = new Mock<IOperation>();
+            operationMock
+                .Setup(m => m.RunAsync(It.IsAny<CancellationToken>()))
+                .Verifiable();
+            operationsFactoryMock
+                .Setup(m => m.CreateMoveOperation(It.IsAny<IList<BinaryFileOperationSettings>>()))
+                .Callback<IList<BinaryFileOperationSettings>>(l =>
+                {
+                    var settings = l.Single();
+                    Assert.Equal(FileName, settings.SourceFilePath);
+                    Assert.Equal(Path.Combine(DirectoryName, FileName), settings.DestinationFilePath);
+                })
+                .Returns(operationMock.Object);
+
+            var directoryServiceMock = new Mock<IDirectoryService>();
+            var fileOpeningServiceMock = new Mock<IFileOpeningService>();
+
+            IOperationsService operationsService = new OperationsService(
+                filesSelectionServiceMock.Object,
+                operationsFactoryMock.Object,
+                directoryServiceMock.Object,
+                fileOpeningServiceMock.Object);
+
+            await operationsService.MoveSelectedFilesAsync(DirectoryName);
+
+            operationMock.Verify(m => m.RunAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task TestFilesCopy()
+        {
+            var filesSelectionServiceMock = new Mock<IFilesSelectionService>();
+            filesSelectionServiceMock
+                .SetupGet(m => m.SelectedFiles)
+                .Returns(new[] {FileName});
+
+            var operationsFactoryMock = new Mock<IOperationsFactory>();
+            var operationMock = new Mock<IOperation>();
+            operationMock
+                .Setup(m => m.RunAsync(It.IsAny<CancellationToken>()))
+                .Verifiable();
+            operationsFactoryMock
+                .Setup(m => m.CreateCopyOperation(It.IsAny<IList<BinaryFileOperationSettings>>()))
+                .Callback<IList<BinaryFileOperationSettings>>(l =>
+                {
+                    var settings = l.Single();
+                    Assert.Equal(FileName, settings.SourceFilePath);
+                    Assert.Equal(Path.Combine(DirectoryName, FileName), settings.DestinationFilePath);
+                })
+                .Returns(operationMock.Object);
+
+            var directoryServiceMock = new Mock<IDirectoryService>();
+            var fileOpeningServiceMock = new Mock<IFileOpeningService>();
+
+            IOperationsService operationsService = new OperationsService(
+                filesSelectionServiceMock.Object,
+                operationsFactoryMock.Object,
+                directoryServiceMock.Object,
+                fileOpeningServiceMock.Object);
+
+            await operationsService.CopySelectedFilesAsync(DirectoryName);
+
+            operationMock.Verify(m => m.RunAsync(It.IsAny<CancellationToken>()), Times.Once());
+        }
+
+        [Fact]
+        public void TestDirectoryCreation()
+        {
+            var filesSelectionServiceMock = new Mock<IFilesSelectionService>();
+            var operationsFactoryMock = new Mock<IOperationsFactory>();
+            var directoryServiceMock = new Mock<IDirectoryService>();
+            directoryServiceMock
+                .SetupGet(m => m.SelectedDirectory)
+                .Returns(SelectedDirectoryName);
+            var fullDirectoryPath = Path.Combine(SelectedDirectoryName, DirectoryName);
+            directoryServiceMock
+                .Setup(m => m.CreateDirectory(fullDirectoryPath))
+                .Verifiable();
+            var fileOpeningServiceMock = new Mock<IFileOpeningService>();
+
+            IOperationsService operationsService = new OperationsService(
+                filesSelectionServiceMock.Object,
+                operationsFactoryMock.Object,
+                directoryServiceMock.Object,
+                fileOpeningServiceMock.Object);
+
+            operationsService.CreateDirectory(DirectoryName);
+
+            directoryServiceMock.Verify(m => m.CreateDirectory(fullDirectoryPath), Times.Once());
+        }
+    }
+}
