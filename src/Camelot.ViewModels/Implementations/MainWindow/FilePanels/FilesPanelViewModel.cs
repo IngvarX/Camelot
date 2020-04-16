31e838b75f0344b77d6e548c33cf714a48edc836
ingvar1995 <igorn1995@yandex.ru>
2020-04-16 23:55:17 +0300

    Addded arrow for data grid header

--- src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/FilesPanelViewModel.cs
+++ src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/FilesPanelViewModel.cs
@@ -63,14 +63,29 @@ namespace Camelot.ViewModels.Implementations.MainWindow.FilePanels
         public SortingColumn SortingColumn
         {
             get => _sortingColumn;
-            set => this.RaiseAndSetIfChanged(ref _sortingColumn, value);
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _sortingColumn, value);
+                this.RaisePropertyChanged(nameof(IsSortingByNameEnabled));
+                this.RaisePropertyChanged(nameof(IsSortingByDateEnabled));
+                this.RaisePropertyChanged(nameof(IsSortingByExtensionEnabled));
+                this.RaisePropertyChanged(nameof(IsSortingBySizeEnabled));
+            }
         }
-        
+
         public bool IsSortingByAscendingEnabled
         {
             get => _isSortingByAscendingEnabled;
             set => this.RaiseAndSetIfChanged(ref _isSortingByAscendingEnabled, value);
         }
+
+        public bool IsSortingByNameEnabled => SortingColumn == SortingColumn.Name;
+        
+        public bool IsSortingByDateEnabled => SortingColumn == SortingColumn.Date;
+        
+        public bool IsSortingByExtensionEnabled => SortingColumn == SortingColumn.Extension;
+        
+        public bool IsSortingBySizeEnabled => SortingColumn == SortingColumn.Size;
 
         public IEnumerable<IFileSystemNodeViewModel> FileSystemNodes => _fileSystemNodes;
 
---
+++ src/Camelot/Views/Main/DataGridHeaderArrowView.xaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Camelot.Views.Main
+{
+    public class DataGridHeaderArrowView :  UserControl
+    {
+        public DataGridHeaderArrowView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
