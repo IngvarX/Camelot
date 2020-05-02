04326947bb453e0d531d705d7313bf6d923fb10b
ingvar1995 <igorn1995@yandex.ru>
2020-05-02 14:12:08 +0300

    Fixed opening of executables

--- src/Camelot.Services/Implementations/FileOpeningService.cs
+++ src/Camelot.Services/Implementations/FileOpeningService.cs
@@ -2,6 +2,7 @@ using System;
 using Camelot.Services.Environment.Enums;
 using Camelot.Services.Environment.Interfaces;
 using Camelot.Services.Interfaces;
+using Mono.Unix;
 
 namespace Camelot.Services.Implementations
 {
@@ -28,11 +29,31 @@ namespace Camelot.Services.Implementations
                 return;
             }
 
+            if (CheckIfExecutable(resource))
+            {
+                _processService.Run(resource);
+
+                return;
+            }
+
             var command = GetCommand(platform);
             var arguments = $"\"{resource}\"";
 
             _processService.Run(command, arguments);
         }
+
+        private static bool CheckIfExecutable(string resource)
+        {
+            var fileInfo = new UnixFileInfo(resource);
+            var permissions = fileInfo.FileAccessPermissions;
+
+            return CheckIfFlagIsSet(permissions, FileAccessPermissions.GroupExecute)
+                   || CheckIfFlagIsSet(permissions, FileAccessPermissions.UserExecute)
+                   || CheckIfFlagIsSet(permissions, FileAccessPermissions.OtherExecute);
+        }
+
+        private static bool CheckIfFlagIsSet(FileAccessPermissions permissions, FileAccessPermissions flag) =>
+            (permissions & flag) == flag;
 
         private static string GetCommand(Platform platform)
         {
