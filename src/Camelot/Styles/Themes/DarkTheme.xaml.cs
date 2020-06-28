f34f283387497c792233ae1d761b512f0950cc1c
IngvarX <igorn1995@yandex.ru>
2020-06-28 23:25:04 +0300

    Moved styles from dark theme xaml, dynamic load of styles on start

---
+++ src/Camelot/Styles/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Markup.Xaml;
+
+namespace Camelot.Styles.Themes
+{
+    public class DarkTheme : Avalonia.Styling.Styles
+    {
+        public DarkTheme() => AvaloniaXamlLoader.Load(this);
+    }
+}
