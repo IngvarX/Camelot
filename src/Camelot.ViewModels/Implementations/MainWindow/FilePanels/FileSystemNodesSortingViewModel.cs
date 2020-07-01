822cd4ab77804cce40eb2948bcd92399e54087b7
IngvarX <igorn1995@yandex.ru>
2020-07-02 00:47:21 +0300

    FileSystemNodesSortingViewModel cleanup, converters refactoring

--- src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/FileSystemNodesSortingViewModel.cs
+++ src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/FileSystemNodesSortingViewModel.cs
@@ -7,33 +7,18 @@ namespace Camelot.ViewModels.Implementations.MainWindow.FilePanels
     {
         private SortingColumn _sortingColumn;
         private bool _isSortingByAscendingEnabled;
-        
+
         public SortingColumn SortingColumn
         {
             get => _sortingColumn;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _sortingColumn, value);
-                this.RaisePropertyChanged(nameof(IsSortingByNameEnabled));
-                this.RaisePropertyChanged(nameof(IsSortingByDateEnabled));
-                this.RaisePropertyChanged(nameof(IsSortingByExtensionEnabled));
-                this.RaisePropertyChanged(nameof(IsSortingBySizeEnabled));
-            }
+            set => this.RaiseAndSetIfChanged(ref _sortingColumn, value);
         }
-        
+
         public bool IsSortingByAscendingEnabled
         {
             get => _isSortingByAscendingEnabled;
             private set => this.RaiseAndSetIfChanged(ref _isSortingByAscendingEnabled, value);
         }
-
-        public bool IsSortingByNameEnabled => SortingColumn == SortingColumn.Name;
-        
-        public bool IsSortingByDateEnabled => SortingColumn == SortingColumn.Date;
-        
-        public bool IsSortingByExtensionEnabled => SortingColumn == SortingColumn.Extension;
-        
-        public bool IsSortingBySizeEnabled => SortingColumn == SortingColumn.Size;
 
         public FileSystemNodesSortingViewModel(
             SortingColumn sortingColumn,
@@ -42,7 +27,7 @@ namespace Camelot.ViewModels.Implementations.MainWindow.FilePanels
             _sortingColumn = sortingColumn;
             _isSortingByAscendingEnabled = isSortingByAscendingEnabled;
         }
-        
+
         public void ToggleSortingDirection() => IsSortingByAscendingEnabled = !IsSortingByAscendingEnabled;
     }
 }
---
+++ tests/Camelot.ViewModels.Tests/FileSystemNodesSortingViewModelTests.cs
@@ -0,0 +1,24 @@
+using Camelot.ViewModels.Implementations.MainWindow.FilePanels;
+using Xunit;
+
+namespace Camelot.ViewModels.Tests
+{
+    public class FileSystemNodesSortingViewModelTests
+    {
+        [Theory]
+        [InlineData(SortingColumn.Date, SortingColumn.Extension, true)]
+        [InlineData(SortingColumn.Name, SortingColumn.Size, false)]
+        public void Test(SortingColumn sortingColumn, SortingColumn newSortingColumn, bool isSortingByAscendingEnabled)
+        {
+            var viewModel = new FileSystemNodesSortingViewModel(sortingColumn, isSortingByAscendingEnabled);
+            Assert.Equal(sortingColumn, viewModel.SortingColumn);
+            Assert.Equal(isSortingByAscendingEnabled, viewModel.IsSortingByAscendingEnabled);
+
+            viewModel.SortingColumn = newSortingColumn;
+            Assert.Equal(newSortingColumn, viewModel.SortingColumn);
+
+            viewModel.ToggleSortingDirection();
+            Assert.Equal(!isSortingByAscendingEnabled, viewModel.IsSortingByAscendingEnabled);
+        }
+    }
+}
