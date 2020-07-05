c0aa06733e12cdb8a459b73f8bd066c7a64439ef
Eduard Paprotski <fisheroliver9@gmail.com>
2020-07-05 14:12:32 +0300

    feat: Show native name in brackets

--- src/Camelot.ViewModels/Implementations/Settings/LanguageSettingsViewModel.cs
+++ src/Camelot.ViewModels/Implementations/Settings/LanguageSettingsViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using Camelot.Services.Abstractions;
 using Camelot.Services.Abstractions.Models;
 using Camelot.ViewModels.Interfaces.Settings;
-using DynamicData;
 using ReactiveUI;
 
 namespace Camelot.ViewModels.Implementations.Settings
@@ -15,7 +13,7 @@ namespace Camelot.ViewModels.Implementations.Settings
         private readonly ILanguageManager _languageManager;
 
         private LanguageModel _currentLanguage;
-        private readonly ObservableCollection<LanguageModel> _allLanguages;
+        private readonly ObservableCollection<LanguageModel> _languages;
         private bool _isActivated;
 
         public LanguageModel CurrentLanguage
@@ -24,7 +22,7 @@ namespace Camelot.ViewModels.Implementations.Settings
             set => this.RaiseAndSetIfChanged(ref _currentLanguage, value);
         }
 
-        public IEnumerable<LanguageModel> AllLanguages => _allLanguages;
+        public IEnumerable<LanguageModel> Languages => _languages;
 
         public LanguageSettingsViewModel(
             ILocalizationService localizationService, 
@@ -33,7 +31,7 @@ namespace Camelot.ViewModels.Implementations.Settings
             _localizationService = localizationService;
             _languageManager = languageManager;
 
-            _allLanguages = SetupLanguages();
+            _languages = SetupLanguages();
         }
 
         public bool IsChanged => _currentLanguage != CurrentLanguage;
