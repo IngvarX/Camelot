244e21a304df47cb8da3f8c00e0621e76ddb4292
IngvarX <igorn1995@yandex.ru>
2020-07-07 23:53:50 +0300

    Removed models from codecov, added test for file panel activation/deactivation

--- tests/Camelot.ViewModels.Tests/FilesPanelViewModelTests.cs
+++ tests/Camelot.ViewModels.Tests/FilesPanelViewModelTests.cs
@@ -195,7 +195,7 @@ namespace Camelot.ViewModels.Tests
                 .Returns(false);
             tabViewModelMock
                 .SetupGet(m => m.SortingViewModel)
-                .Returns(sortingModelMock.Object);;
+                .Returns(sortingModelMock.Object);
             _autoMocker
                 .Setup<ITabViewModelFactory, ITabViewModel>(m => m.Create(It.IsAny<TabModel>()))
                 .Returns(tabViewModelMock.Object);
@@ -218,9 +218,6 @@ namespace Camelot.ViewModels.Tests
             _autoMocker
                 .Setup<IFileService, IReadOnlyCollection<FileModel>>(m => m.GetFiles(AppRootDirectory))
                 .Returns(new[] {new FileModel {Name = File}});
-            _autoMocker
-                .Setup<IFileService, IReadOnlyCollection<FileModel>>(m => m.GetFiles(NewDirectory))
-                .Returns(new FileModel[] {});
             const string formattedSize = "42";
             _autoMocker
                 .Setup<IFileSizeFormatter, string>(m => m.GetFormattedSize(size))
@@ -253,6 +250,62 @@ namespace Camelot.ViewModels.Tests
 
             filesPanelViewModel.SelectedFileSystemNodes.RemoveAt(0);
             CheckSelection(0, 0);
+        }
+
+        [Fact]
+        public void TestActivationAndDeactivation()
+        {
+            _autoMocker
+                .Setup<IFileSystemNodeViewModelFactory, IFileSystemNodeViewModel>(m => m.Create(It.IsAny<DirectoryModel>(), It.IsAny<bool>()))
+                .Returns(new Mock<IDirectoryViewModel>().Object);
+            var tabViewModelMock = new Mock<ITabViewModel>();
+            tabViewModelMock
+                .SetupGet(m => m.CurrentDirectory)
+                .Returns(AppRootDirectory);
+            tabViewModelMock
+                .SetupSet(m => m.IsGloballyActive = true)
+                .Verifiable();
+            tabViewModelMock
+                .SetupSet(m => m.IsGloballyActive = false)
+                .Verifiable();
+            _autoMocker
+                .Setup<ITabViewModelFactory, ITabViewModel>(m => m.Create(It.IsAny<TabModel>()))
+                .Returns(tabViewModelMock.Object);
+            _autoMocker
+                .Setup<IFileSystemNodeViewModelComparerFactory, IComparer<IFileSystemNodeViewModel>>(m => m.Create(It.IsAny<IFileSystemNodesSortingViewModel>()))
+                .Returns(new Mock<IComparer<IFileSystemNodeViewModel>>().Object);
+            _autoMocker
+                .Setup<IFilesPanelStateService, PanelModel>(m => m.GetPanelState())
+                .Returns(new PanelModel
+                {
+                    Tabs = new List<TabModel> {new TabModel {Directory = AppRootDirectory}}
+                });
+            _autoMocker
+                .Setup<IDirectoryService, bool>(m => m.CheckIfExists(AppRootDirectory))
+                .Returns(true);
+            _autoMocker
+                .Setup<IDirectoryService, IReadOnlyCollection<DirectoryModel>>(m =>
+                    m.GetChildDirectories(It.IsAny<string>()))
+                .Returns(new[] {new DirectoryModel {Name = NewDirectory}});
+            _autoMocker
+                .Setup<IFileService, IReadOnlyCollection<FileModel>>(m => m.GetFiles(AppRootDirectory))
+                .Returns(new[] {new FileModel {Name = File}});
+
+            var filesPanelViewModel = _autoMocker.CreateInstance<FilesPanelViewModel>();
+            var isActivationCallbackCalled = false;
+            filesPanelViewModel.ActivatedEvent += (sender, args) => isActivationCallbackCalled = true;
+
+            Assert.True(filesPanelViewModel.ActivateCommand.CanExecute(null));
+            filesPanelViewModel.ActivateCommand.Execute(null);
+            Assert.True(isActivationCallbackCalled);
+            tabViewModelMock.VerifySet(m => m.IsGloballyActive = true, Times.AtLeastOnce);
+
+            var isDeactivationCallbackCalled = false;
+            filesPanelViewModel.DeactivatedEvent += (sender, args) => isDeactivationCallbackCalled = true;
+            filesPanelViewModel.Deactivate();
+            Assert.True(isDeactivationCallbackCalled);
+            tabViewModelMock.VerifySet(m => m.IsGloballyActive = false, Times.Once);
+            Assert.Empty(filesPanelViewModel.SelectedFileSystemNodes);
         }
     }
 }
