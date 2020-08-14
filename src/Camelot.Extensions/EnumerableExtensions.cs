d9026ca377b75809bb7b60e4f6754b78ee97f5c0
IngvarX <igorn1995@yandex.ru>
2020-08-15 01:47:18 +0300

    Fixed mistake in codecov excludes and nullable ref types enabled in extensions

--- src/Camelot.Extensions/EnumerableExtensions.cs
+++ src/Camelot.Extensions/EnumerableExtensions.cs
@@ -8,34 +8,14 @@ namespace Camelot.Extensions
     {
         public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
         {
-            if (collection is null)
-            {
-                throw new ArgumentNullException(nameof(collection));
-            }
-            
-            if (action is null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-            
             foreach (var item in collection)
             {
                 action(item);
             }
         }
-        
+
         public static async Task ForEachAsync<T>(this IEnumerable<T> collection, Func<T, Task> action)
         {
-            if (collection is null)
-            {
-                throw new ArgumentNullException(nameof(collection));
-            }
-            
-            if (action is null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-            
             foreach (var item in collection)
             {
                 await action(item);
--- src/Camelot.Extensions/EventExtensions.cs
+++ src/Camelot.Extensions/EventExtensions.cs
@@ -6,20 +6,10 @@ namespace Camelot.Extensions
     public static class EventExtensions
     {
         public static void Raise<TEventArgs>(
-            this EventHandler<TEventArgs> eventHandler,
+            this EventHandler<TEventArgs>? eventHandler,
             object sender,
             TEventArgs args) where TEventArgs : EventArgs
         {
-            if (sender is null)
-            {
-                throw new ArgumentNullException(nameof(sender));
-            }
-
-            if (args is null)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
-
             var handler = Volatile.Read(ref eventHandler);
 
             handler?.Invoke(sender, args);
