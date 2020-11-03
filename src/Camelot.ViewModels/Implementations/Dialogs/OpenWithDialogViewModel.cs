83346abad9b9f37747b714358472eb1248418c6a
Edwok <fisheroliver9@gmail.com>
2020-11-03 17:36:39 +0300

    improve: show recommended apps for windows

--- src/Camelot.ViewModels/Implementations/Dialogs/OpenWithDialogViewModel.cs
+++ src/Camelot.ViewModels/Implementations/Dialogs/OpenWithDialogViewModel.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Camelot.Services.Abstractions;
 using Camelot.Services.Abstractions.Models;
 using Camelot.ViewModels.Implementations.Dialogs.NavigationParameters;
+using DynamicData;
 using ReactiveUI.Fody.Helpers;
 
 namespace Camelot.ViewModels.Implementations.Dialogs
@@ -12,7 +13,9 @@ namespace Camelot.ViewModels.Implementations.Dialogs
     {
         private readonly IApplicationService _applicationService;
 
-        public IEnumerable<ApplicationModel> Applications { get; private set; }
+        private readonly ObservableCollection<ApplicationModel> _recommendedApplications;
+
+        public IEnumerable<ApplicationModel> RecommendedApplications => _recommendedApplications;
 
         [Reactive]
         public string OpenFileExtension { get; set; }
@@ -20,12 +23,14 @@ namespace Camelot.ViewModels.Implementations.Dialogs
         public OpenWithDialogViewModel(IApplicationService applicationService)
         {
             _applicationService = applicationService;
+            _recommendedApplications = new ObservableCollection<ApplicationModel>();
         }
 
         public override async Task ActivateAsync(OpenWithNavigationParameter parameter)
         {
             OpenFileExtension = parameter.FileExtension;
-            Applications = new ObservableCollection<ApplicationModel>(await _applicationService.GetAssociatedApplications(OpenFileExtension));
+
+            _recommendedApplications.AddRange(await _applicationService.GetAssociatedApplications(OpenFileExtension));
         }
     }
 }
