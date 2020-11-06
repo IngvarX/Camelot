da13d2330b21965e4c4398657fa27ca5b11f2c1c
Edwok <fisheroliver9@gmail.com>
2020-11-06 21:50:46 +0300

    feat: add simple test for windows application service

---
+++ tests/Camelot.Services.Windows.Tests/WindowsApplicationServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Camelot.Services.Windows.Tests
+{
+    public class WindowsApplicationServiceTests
+    {
+        [Theory]
+        [InlineData(".txt")]
+        [InlineData(".xml")]
+        public async Task TestGetAssociatedApplications(string fileExtension)
+        {
+            var applicationService = new WindowsApplicationService();
+
+            var associatedApplications = await applicationService.GetAssociatedApplications(fileExtension);
+
+            Assert.NotNull(associatedApplications);
+            Assert.True(associatedApplications.Any());
+        }
+
+        [Fact]
+        public async Task TestGetInstalledApplications()
+        {
+            var applicationService = new WindowsApplicationService();
+
+            var associatedApplications = await applicationService.GetInstalledApplications();
+
+            Assert.NotNull(associatedApplications);
+            Assert.True(associatedApplications.Any());
+        }
+    }
+}
