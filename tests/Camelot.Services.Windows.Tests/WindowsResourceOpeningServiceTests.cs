88c9840a584c4958e73079cb22d52aa40badc351
IngvarX <igorn1995@yandex.ru>
2020-11-10 23:27:57 +0300

    WindowsResourceOpeningServiceTests fixed

--- tests/Camelot.Services.Windows.Tests/WindowsResourceOpeningServiceTests.cs
+++ tests/Camelot.Services.Windows.Tests/WindowsResourceOpeningServiceTests.cs
@@ -7,22 +7,22 @@ namespace Camelot.Services.Windows.Tests
 {
     public class WindowsResourceOpeningServiceTests
     {
+        private const string Command = "explorer";
+
         private readonly AutoMocker _autoMocker;
 
         public WindowsResourceOpeningServiceTests()
         {
             _autoMocker = new AutoMocker();
         }
-        
-        [Fact]
-        public void TestFileServiceOpeningWindows()
-        {
-            const string fileName = "File.txt";
-            const string command = "explorer";
-            var arguments = $"\"{fileName}\"";
 
+        [Theory]
+        [InlineData("File.txt", "File.txt")]
+        [InlineData("Fi le.txt", "\"Fi le.txt\"")]
+        public void TestFileServiceOpeningWindows(string fileName, string arguments)
+        {
             _autoMocker
-                .Setup<IProcessService>(m => m.Run(command, arguments))
+                .Setup<IProcessService>(m => m.Run(Command, arguments))
                 .Verifiable();
 
             var fileOpeningService = _autoMocker.CreateInstance<WindowsResourceOpeningService>();
@@ -30,7 +30,7 @@ namespace Camelot.Services.Windows.Tests
             fileOpeningService.Open(fileName);
 
             _autoMocker
-                .Verify<IProcessService>(m => m.Run(command, arguments), Times.Once);
+                .Verify<IProcessService>(m => m.Run(Command, arguments), Times.Once);
         }
     }
 }
