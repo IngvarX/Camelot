b315c92d01823d774dbbe579e3dae72935944513
IngvarX <igorn1995@yandex.ru>
2020-11-28 22:41:52 +0300

    Application view moved to separate file

---
+++ src/Camelot/Views/Dialogs/OpenWith/ApplicationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Camelot.Views.Dialogs.OpenWith
+{
+    public class ApplicationView : UserControl
+    {
+        public ApplicationView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+    }
+}
