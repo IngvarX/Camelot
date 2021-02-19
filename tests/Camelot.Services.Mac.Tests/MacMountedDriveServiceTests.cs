cf944d2509f7194b5b296480ca4c0e80bda88448
IngvarX <igorn1995@yandex.ru>
2021-02-19 19:47:04 +0300

    Mac unmount feature tests

---
+++ tests/Camelot.Services.Mac.Tests/MacMountedDriveServiceTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Camelot.Services.Environment.Interfaces;
+using Camelot.Services.Environment.Models;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Camelot.Services.Mac.Tests
+{
+    public class MacMountedDriveServiceTests
+    {
+        private readonly AutoMocker _autoMocker;
+
+        public MacMountedDriveServiceTests()
+        {
+            _autoMocker = new AutoMocker();
+        }
+
+        [Theory]
+        [InlineData("/home/test", "diskutil", "unmount /home/test")]
+        [InlineData("/home/camelot", "diskutil", "unmount /home/camelot")]
+        [InlineData("/dev/disk1", "diskutil", "unmount /dev/disk1")]
+        public void TestUnmount(string drive, string command, string arguments)
+        {
+            _autoMocker
+                .Setup<IProcessService>(m => m.Run(command, arguments))
+                .Verifiable();
+            _autoMocker
+                .Setup<IEnvironmentDriveService, IReadOnlyList<DriveInfo>>(m => m.GetMountedDrives())
+                .Returns(Array.Empty<DriveInfo>());
+
+            var service = _autoMocker.CreateInstance<MacMountedDriveService>();
+            service.Unmount(drive);
+
+            _autoMocker
+                .Verify<IProcessService>(m => m.Run(command, arguments), Times.Once);
+        }
+    }
+}
