c7a224edd622faf0d36537930013b6af30c09ce5
IngvarX <igorn1995@yandex.ru>
2021-03-04 18:29:58 +0300

    LinuxMountedDriveService tests added

--- tests/Camelot.Services.Linux.Tests/LinuxMountedDriveServiceTests.cs
+++ tests/Camelot.Services.Linux.Tests/LinuxMountedDriveServiceTests.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Camelot.Services.Environment.Interfaces;
 using Camelot.Services.Environment.Models;
+using Camelot.Services.Linux.Interfaces;
+using Camelot.Tests.Common.Extensions;
 using Moq;
 using Moq.AutoMock;
 using Xunit;
@@ -35,6 +38,63 @@ namespace Camelot.Services.Linux.Tests
 
             _autoMocker
                 .Verify<IProcessService>(m => m.Run(command, arguments), Times.Once);
+        }
+
+        [Fact]
+        public void TestUnmountException()
+        {
+            _autoMocker
+                .Setup<IProcessService>(m => m.Run(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<InvalidOperationException>();
+            _autoMocker
+                .Setup<IEnvironmentDriveService, IReadOnlyList<DriveInfo>>(m => m.GetMountedDrives())
+                .Returns(Array.Empty<DriveInfo>());
+            _autoMocker.MockLogError();
+
+            var service = _autoMocker.CreateInstance<LinuxMountedDriveService>();
+            service.Unmount("/media/camelot/test");
+
+            _autoMocker.VerifyLogError(Times.Once());
+        }
+
+        [Theory]
+        [InlineData("/home/test", "/dev/sdc1", "eject", "/dev/sdc1")]
+        [InlineData("/home/camelot", "/dev/sdc2", "eject", "/dev/sdc2")]
+        [InlineData("/home/sda1", "/dev/sdc3", "eject", "/dev/sdc3")]
+        public async Task TestEject(string rootDirectory, string driveName, string command, string arguments)
+        {
+            _autoMocker
+                .Setup<IProcessService>(m => m.Run(command, arguments))
+                .Verifiable();
+            _autoMocker
+                .Setup<IEnvironmentDriveService, IReadOnlyList<DriveInfo>>(m => m.GetMountedDrives())
+                .Returns(Array.Empty<DriveInfo>());
+            _autoMocker
+                .Setup<IDriveNameService, Task<string>>(m => m.GetDriveNameAsync(rootDirectory))
+                .ReturnsAsync(driveName);
+
+            var service = _autoMocker.CreateInstance<LinuxMountedDriveService>();
+            await service.EjectAsync(rootDirectory);
+
+            _autoMocker
+                .Verify<IProcessService>(m => m.Run(command, arguments), Times.Once);
+        }
+
+        [Fact]
+        public async Task TestEjectException()
+        {
+            _autoMocker
+                .Setup<IProcessService>(m => m.Run(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<InvalidOperationException>();
+            _autoMocker
+                .Setup<IEnvironmentDriveService, IReadOnlyList<DriveInfo>>(m => m.GetMountedDrives())
+                .Returns(Array.Empty<DriveInfo>());
+            _autoMocker.MockLogError();
+
+            var service = _autoMocker.CreateInstance<LinuxMountedDriveService>();
+            await service.EjectAsync("/media/camelot/test");
+
+            _autoMocker.VerifyLogError(Times.Once());
         }
     }
 }
