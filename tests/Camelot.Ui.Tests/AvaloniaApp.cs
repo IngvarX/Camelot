2619915d72a6c39ffdedc0e0d3be477e3ca83948
IngvarX <igorn1995@yandex.ru>
2021-03-18 19:14:33 +0300

    Initial commit for UI tests

---
+++ tests/Camelot.Ui.Tests/AvaloniaApp.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Headless;
+using Avalonia.ReactiveUI;
+using Camelot.DependencyInjection;
+using Splat;
+
+namespace Camelot.Ui.Tests
+{
+    public static class AvaloniaApp
+    {
+        public static void RegisterDependencies() =>
+            Bootstrapper.Register(Locator.CurrentMutable, Locator.Current);
+
+        public static Window GetMainWindow() => GetApp().MainWindow;
+
+        public static IClassicDesktopStyleApplicationLifetime GetApp() =>
+            (IClassicDesktopStyleApplicationLifetime) Application.Current.ApplicationLifetime;
+
+        public static void Stop()
+        {
+            var app = GetApp();
+            if (app is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+
+            app.Shutdown();
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            var type = typeof(AppBuilderBase<AppBuilder>);
+            var field = type.GetField("s_setupWasAlreadyCalled", BindingFlags.NonPublic | BindingFlags.Static);
+            field.SetValue(null, false);
+
+            return AppBuilder
+                .Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI();
+        }
+    }
+}
