9ba190c567e41314ffaa6c8ca513d129b4309c79
IngvarX <igorn1995@yandex.ru>
2021-03-27 20:49:03 +0300

    Suggestion view moved to separate file

---
+++ src/Camelot/Views/Main/Controls/SuggestionView.xaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Camelot.Views.Main.Controls
+{
+    public class SuggestionView : UserControl
+    {
+        public SuggestionView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+    }
+}
