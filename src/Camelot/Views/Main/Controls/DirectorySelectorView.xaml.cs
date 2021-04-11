33578f0092cefadbe01b3745972caf01667bce6b
IngvarX <igorn1995@yandex.ru>
2021-04-11 19:25:03 +0300

    Created dir selector view

---
+++ src/Camelot/Views/Main/Controls/DirectorySelectorView.xaml.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using Camelot.ViewModels.Interfaces.MainWindow.FilePanels;
+
+namespace Camelot.Views.Main.Controls
+{
+    public class DirectorySelectorView : UserControl
+    {
+        private ListBox SuggestionsListBox => this.FindControl<ListBox>("SuggestionsListBox");
+
+        public TextBox DirectoryTextBox => this.FindControl<TextBox>("DirectoryTextBox");
+
+        private IFilesPanelViewModel ViewModel => (IFilesPanelViewModel) DataContext;
+
+        public DirectorySelectorView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+        private void SuggestionViewOnTapped(object sender, RoutedEventArgs e) =>
+            SelectDirectory(((IDataContextProvider) sender).DataContext);
+
+        private void DirectoryTextBoxOnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Down && e.Key != Key.Escape)
+            {
+                return;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                SuggestionsListBox.SelectedIndex = 0;
+                var topItem = SuggestionsListBox
+                    .GetVisualDescendants()
+                    .OfType<SuggestionView>()
+                    .FirstOrDefault();
+                topItem?.Focus();
+            }
+            else
+            {
+                HideSuggestionsPopup();
+            }
+
+            e.Handled = true;
+        }
+
+        private void SuggestionsListBoxOnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    SelectDirectory(SuggestionsListBox.SelectedItem);
+                    break;
+                case Key.Up when SuggestionsListBox.SelectedIndex == 0:
+                    SuggestionsListBox.SelectedItem = null;
+                    DirectoryTextBox.Focus();
+                    break;
+                case Key.Escape:
+                    HideSuggestionsPopup();
+                    break;
+            }
+        }
+
+        private void SelectDirectory(object sender)
+        {
+            var viewModel = (ISuggestedPathViewModel) sender;
+
+            DirectoryTextBox.Text = viewModel.FullPath;
+            DirectoryTextBox.CaretIndex = DirectoryTextBox.Text.Length;
+            DirectoryTextBox.Focus();
+        }
+
+        private void HideSuggestionsPopup() => ViewModel.ShouldShowSuggestions = false;
+    }
+}
--- src/Camelot/Views/Main/FilesPanelView.xaml.cs
+++ src/Camelot/Views/Main/FilesPanelView.xaml.cs
@@ -26,9 +26,7 @@ namespace Camelot.Views.Main
 
         private DataGrid FilesDataGrid => this.FindControl<DataGrid>("FilesDataGrid");
 
-        private TextBox DirectoryTextBox => this.FindControl<TextBox>("DirectoryTextBox");
-
-        private ListBox SuggestionsListBox => this.FindControl<ListBox>("SuggestionsListBox");
+        private DirectorySelectorView DirectorySelectorView => this.FindControl<DirectorySelectorView>("DirectorySelectorView");
 
         private IFilesPanelViewModel ViewModel => (IFilesPanelViewModel) DataContext;
 
@@ -46,6 +44,7 @@ namespace Camelot.Views.Main
         {
             ViewModel.Deactivated += ViewModelOnDeactivated;
             ViewModel.Activated += ViewModelOnActivated;
+            DirectorySelectorView.DirectoryTextBox.GotFocus += OnDirectoryTextBoxGotFocus;
 
             FilesDataGrid.AddHandler(DragDrop.DropEvent, OnDrop);
         }
@@ -54,7 +53,7 @@ namespace Camelot.Views.Main
 
         private void ViewModelOnActivated(object sender, EventArgs e)
         {
-            if (!DirectoryTextBox.IsFocused)
+            if (!DirectorySelectorView.DirectoryTextBox.IsFocused)
             {
                 FilesDataGrid.Focus();
             }
@@ -177,61 +176,6 @@ namespace Camelot.Views.Main
         private static void StopEditing(IFileSystemNodeViewModel viewModel) => viewModel.IsEditing = false;
 
         private void ClearSelection() => FilesDataGrid.SelectedItems.Clear();
-
-        private void SuggestionViewOnTapped(object sender, RoutedEventArgs e) =>
-            SelectDirectory(((IDataContextProvider) sender).DataContext);
-
-        private void DirectoryTextBoxOnKeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key != Key.Down && e.Key != Key.Escape)
-            {
-                return;
-            }
-
-            if (e.Key == Key.Down)
-            {
-                SuggestionsListBox.SelectedIndex = 0;
-                var topItem = SuggestionsListBox
-                    .GetVisualDescendants()
-                    .OfType<SuggestionView>()
-                    .FirstOrDefault();
-                topItem?.Focus();
-            }
-            else
-            {
-                HideSuggestionsPopup();
-            }
-
-            e.Handled = true;
-        }
-
-        private void SuggestionsListBoxOnKeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.Key)
-            {
-                case Key.Enter:
-                    SelectDirectory(SuggestionsListBox.SelectedItem);
-                    break;
-                case Key.Up when SuggestionsListBox.SelectedIndex == 0:
-                    SuggestionsListBox.SelectedItem = null;
-                    DirectoryTextBox.Focus();
-                    break;
-                case Key.Escape:
-                    HideSuggestionsPopup();
-                    break;
-            }
-        }
-
-        private void SelectDirectory(object sender)
-        {
-            var viewModel = (ISuggestedPathViewModel) sender;
-
-            DirectoryTextBox.Text = viewModel.FullPath;
-            DirectoryTextBox.CaretIndex = DirectoryTextBox.Text.Length;
-            DirectoryTextBox.Focus();
-        }
-
-        private void HideSuggestionsPopup() => ViewModel.ShouldShowSuggestions = false;
 
         private void PrepareDrag(DataGridCellPointerPressedEventArgs e)
         {
