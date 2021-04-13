952c467b0daeb0c87d2d71edb6c9a6325f768285
IngvarX <igorn1995@yandex.ru>
2021-04-13 18:10:52 +0300

    Fixed tabs drag n drop

--- src/Camelot/Views/Main/Controls/Tabs/TabView.xaml.cs
+++ src/Camelot/Views/Main/Controls/Tabs/TabView.xaml.cs
@@ -1,15 +1,24 @@
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Camelot.Avalonia.Interfaces;
+using Camelot.DependencyInjection;
 using Camelot.ViewModels.Interfaces.MainWindow.FilePanels.Tabs;
+using Splat;
 
 namespace Camelot.Views.Main.Controls.Tabs
 {
     public class TabView : UserControl
     {
+        private const int DragAndDropDelay = 200;
         private const string DataFormat = "Tab";
 
+        private Grid Grid => this.Find<Grid>("TabGrid");
+
         private ITabViewModel ViewModel => (ITabViewModel) DataContext;
+
+        private bool _isGridPressed;
 
         public TabView()
         {
@@ -33,15 +42,34 @@ namespace Camelot.Views.Main.Controls.Tabs
 
         private void SetupDragAndDrop()
         {
-            var grid = this.Find<Grid>("TabGrid");
-            grid.PointerPressed += DoDrag;
+            Grid.PointerPressed += PrepareDrag;
 
             AddHandler(DragDrop.DropEvent, OnDrop);
         }
 
-        private async void DoDrag(object sender, PointerPressedEventArgs e)
+        private void PrepareDrag(object sender, PointerPressedEventArgs e)
         {
             if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                return;
+            }
+
+            _isGridPressed = true;
+            Grid.PointerReleased += GridOnPointerReleased;
+
+            Task.Delay(DragAndDropDelay).ContinueWith(_ =>
+            {
+                var dispatcher = Locator.Current.GetRequiredService<IApplicationDispatcher>();
+                dispatcher.DispatchAsync(() => DoDragAsync(e));
+            });
+        }
+
+        private async Task DoDragAsync(PointerEventArgs e)
+        {
+            var button = this.Find<Button>("TabButton");
+            button.Classes.Set(":pressed", false);
+
+            if (!_isGridPressed)
             {
                 return;
             }
@@ -51,10 +79,13 @@ namespace Camelot.Views.Main.Controls.Tabs
 
             await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
 
-            var button = this.Find<Button>("TabButton");
-            button.Classes.Set(":pressed", false);
-
             ViewModel.ActivateCommand.Execute(null);
+        }
+
+        private void GridOnPointerReleased(object sender, PointerReleasedEventArgs e)
+        {
+            Grid.PointerReleased -= GridOnPointerReleased;
+            _isGridPressed = false;
         }
 
         private void OnDrop(object sender, DragEventArgs e)
