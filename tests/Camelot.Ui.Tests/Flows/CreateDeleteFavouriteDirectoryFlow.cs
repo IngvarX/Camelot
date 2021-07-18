ded456f2231f8fc4070e1e0f87672a947dbdea8c
IngvarX <igorn1995@yandex.ru>
2021-07-17 18:38:57 -0700

    Tabs and fav dirs tests added

---
+++ tests/Camelot.Ui.Tests/Flows/CreateDeleteFavouriteDirectoryFlow.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Camelot.Ui.Tests.Common;
+using Camelot.Ui.Tests.Steps;
+using Camelot.Views.Main.FavouriteDirectories;
+using Xunit;
+
+namespace Camelot.Ui.Tests.Flows
+{
+    public class CreateDeleteFavouriteDirectoryFlow
+    {
+        [Fact(DisplayName = "Toggle favourite directory")]
+        public async Task ToggleFavouriteDirectoryTest()
+        {
+            var window = AvaloniaApp.GetMainWindow();
+
+            await FocusFilePanelStep.FocusFilePanelAsync(window);
+
+            Assert.Equal(1, GetFavouriteDirectoriesCount(window));
+            Assert.Equal(2, GetFavouriteDirectoriesActiveIconsCount(window));
+
+            ToggleFavouriteDirectoryStep.ToggleFavouriteDirectory(window);
+            await WaitService.WaitForConditionAsync(() => GetFavouriteDirectoriesCount(window) == 0);
+            await WaitService.WaitForConditionAsync(() => GetFavouriteDirectoriesActiveIconsCount(window) == 0);
+
+            ToggleFavouriteDirectoryStep.ToggleFavouriteDirectory(window);
+            await WaitService.WaitForConditionAsync(() => GetFavouriteDirectoriesCount(window) == 1);
+            await WaitService.WaitForConditionAsync(() => GetFavouriteDirectoriesActiveIconsCount(window) == 2);
+        }
+
+        private static int GetFavouriteDirectoriesCount(IVisual window) =>
+            window
+                .GetVisualDescendants()
+                .OfType<FavouriteDirectoryView>()
+                .Count();
+
+        private static int GetFavouriteDirectoriesActiveIconsCount(IVisual window) =>
+            window
+                .GetVisualDescendants()
+                .OfType<Image>()
+                .Count(i => i.Classes.ToHashSet().Contains("favouriteDirectoryImage") && i.IsVisible);
+    }
+}
---
+++ tests/Camelot.Ui.Tests/Steps/ToggleFavouriteDirectoryStep.cs
@@ -0,0 +1,12 @@
+using Avalonia.Input;
+using Camelot.Ui.Tests.Common;
+using Camelot.Views;
+
+namespace Camelot.Ui.Tests.Steps
+{
+    public static class ToggleFavouriteDirectoryStep
+    {
+        public static void ToggleFavouriteDirectory(MainWindow window) =>
+            Keyboard.PressKey(window, Key.D, RawInputModifiers.Control);
+    }
+}
